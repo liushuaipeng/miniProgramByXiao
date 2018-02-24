f32b5a2fba805e72b95ab80f84d2e6a661ffd04b
刘帅鹏 <shuaipengliu@foxmail.com>
2018-02-24 16:49:45 +0800

    服务器图片代理

--- server-dev/server/routes/jw3zb.js
+++ server-dev/server/routes/jw3zb.js
@@ -1,47 +1,56 @@
 const router = require("koa-router")({
-    prefix: "/jw3zb"
+  prefix: "/jw3zb"
 });
 const co = require("co");
+const request = require("request");
 
 const Client = require("../jw3/zb/client");
 const CONFIG = require("../jw3/config");
 const client = new Client(CONFIG.APP_KEY, CONFIG.APP_SECRET);
 
 router.get("/test", ctx => {
-    ctx.state = {
-        code: 1,
-        data: {
-            msg: "剑网3装备数据"
-        }
-    };
+  ctx.state = {
+    code: 1,
+    data: {
+      msg: "剑网3装备数据"
+    }
+  };
 });
 
 router.get("/list", async ctx => {
-    var url = "http://api.j3pz.com/equip";
-    var query = ctx.query;
-    var result = await client.get(url, {
-        query: query,
-        headers: {
-            accept: "application/json"
-        }
-    });
-    ctx.state = {
-        code: 1,
-        data: {
-            list: result.data
-        }
-    };
+  var url = "http://api.j3pz.com/equip";
+  var query = ctx.query;
+  var result = await client.get(url, {
+    query: query,
+    headers: {
+      accept: "application/json"
+    }
+  });
+  ctx.state = {
+    code: 1,
+    data: {
+      list: result.data
+    }
+  };
 });
 router.get("/detail/:id", async ctx => {
-    var url = "http://api.j3pz.com/equip/" + ctx.params.id;
-    var result = await client.get(url, {
-        headers: {
-            accept: "application/json"
-        }
-    });
-    ctx.state = {
-        code: 1,
-        data: result.data
-    };
+  var url = "http://api.j3pz.com/equip/" + ctx.params.id;
+  var result = await client.get(url, {
+    headers: {
+      accept: "application/json"
+    }
+  });
+  ctx.state = {
+    code: 1,
+    data: result.data
+  };
+});
+
+router.get("/icons/:id", async ctx => {
+  var opts = {
+    url: "https://www.j3pz.com/icons/" + ctx.params.id + ".png",
+    strictSSL: false
+  };
+  ctx.body = request.get(opts);
 });
 module.exports = router;
